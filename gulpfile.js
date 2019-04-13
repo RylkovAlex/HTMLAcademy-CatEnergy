0fd4fdbd809000aa7d1add851dac7dbaf4bdf128
RylkovAlex <32511612+RylkovAlex@users.noreply.github.com>
2019-04-13 20:48:22 +0300

    поправил ошибки после npm test

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,14 @@ var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
 var sass = require("gulp-sass");
+// var wait = require("gulp-wait");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
+    // .pipe(wait(200))
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
